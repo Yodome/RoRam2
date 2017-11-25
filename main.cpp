caba549ff056009ded42e39afcb9ded46959d25c
OwDemShad <galo.guccy@gmail.com>
2017-11-25 22:41:02 +0100

    Ajout des commentaires pour la documentation doxy
    A voir la configuration doxy pour afficher les graphiques...

--- main.cpp
+++ main.cpp
@@ -18,44 +18,65 @@ const int EHORIZ = 2;
 const int BOBO = 9;
 const int NJOUEURS = 3;
 
-
+/**
+	Contient un tableau 2D simulant la grille
+*/
 struct Grille
 {
     int tab[NMAXLIGS][NMAXCOLS];
 };
 
+/**
+	Grille permettant de représenter le parcours du robot
+*/
 typedef Grille GrilleVisite;
 
+/**
+	Contient 2 coordonnees x et y simulant une Position
+*/ 
 struct Position
 {
     int x;
     int y;
 };
 
+/**
+	Structure contenant les informations relatives à un parcours du robot
+*/
 struct RParcours
 {
-    int nbObj;
-    int ptsTotal;
-    bool bonus;
+    int nbObj;		// nombre d'objets dans la grille
+    int ptsTotal;	// nombre de points au total récoltés par le parcours du robot pour un joueur
+    bool bonus;		// valeur du bonus si le robot passe par tous les miroirs
 };
 
-
+/**
+	Contient le score et le nombre de joueurs
+*/
 struct Scores
 {
     int score;
     int nbJoueur;
 };
 
+/**
+	Contient tous les paramètres de jeu que l'utilisateur renseigne avant de jouer
+*/
 struct ParamsJeu{
-    int nbLignes;
-    int nbCol;
-    int nbJoueurs;
-    int nbObj;
-    int nbMiroir;
-    int bobo; // valeur du bonus
+    int nbLignes;	// nombre de lignes de la grille
+    int nbCol;		// nombre de colonnes de la grille
+    int nbJoueurs;	// nombre de joueurs pour la partie
+    int nbObj;		// nombre d'objets dans la grille
+    int nbMiroir;	// nombre de miroirs à disposition pour un tour
+    int bobo; 		// valeur du bonus
 };
 
 //-------------------------------------- Modelisation du plateau --------------------------------
+/**
+	Initialise tous les éléments de la grille à 0
+	@param[in] gr - grille à initialisé
+	@param[in] pj - paramètres de jeu (nb lignes/colonnes)
+*/
 void initialiserGrille(Grille &gr,const ParamsJeu &pj)
 {
     for (int i = 0; i < pj.nbLignes; i++)
@@ -70,12 +91,24 @@ void initialiserGrille(Grille &gr,const ParamsJeu &pj)
 
 
 //-------------------------------------- Modelisation d'une position --------------------------------
+/**
+	Indique si une position donnée dans la grille est valide : x et y dans la grille et non en dehors
+	@param[in] pos - position dont on vérifie si les coordonnées sont valides
+	@param[in] gr - la grille dans laquelle on vérifie les coordonnées
+	@param[in] pj - paramètres de jeu (nb lignes/colonnes)
+	@return boolean - true si la position est valide, false sinon
+*/
 bool estValide(const Position &pos,const Grille &gr, const ParamsJeu &pj)
 {
     return ( pos.x <= pj.nbCol && pos.y <= pj.nbLignes);
 }
 
-
+/**
+	Inidique si une position donnée est libre : c'est à dire si la valeur dans la grille est 0
+	@param[in] pos - position dont on vérifie si les coordonnées dans la grille sont libres
+	@param[in] gr - grille dans laquelle on vérifie si la case est libre
+	@return boolean - true si la case est libre
+*/
 bool estLibre(const Position &pos,const Grille &gr)
 {
     return gr.tab[pos.y][pos.x] == 0;
@@ -84,16 +117,34 @@ bool estLibre(const Position &pos,const Grille &gr)
 
 
 //-------------------------------------- Initialisation de la grille --------------------------------
+/**
+	Met à jour la grille aux coordonnées de la Position selon la valeur val
+	@param[in] pos - position qui sert à changer une valeur dans la grille selon cette position
+	@param[in,out] gr - grille passée en paramètre dont on change la valeur aux coordonnées de la position selon la valeur val
+	@param[in] val - valeur que prend la case de la grille
+*/
 void majGrille(const Position &pos, Grille &gr, int val)
 {
     gr.tab[pos.y][pos.x] = val;
 }
 
+/**
+	Renvoie un nombre aléatoire compris dans l'intervalle [min ; max[
+	@param[in] min - valeur inférieure de l'intervalle
+	@param[in] max - valeur supérieure de l'intervalle
+	@return int - retourne un nombre entier aléatoire
+*/
 int aleatoire(int min, int max)
 {
     return rand()%(max-min)+min;// intervalle [a;b[
 }
 
+/**
+	Permet de poser un objet alétoire à un endroit aléatoire dans la grille
+	@param[in,out] gr - grille dans laquelle on pose un objet
+	@param[in] pos - coordonnées auxquelles poser un objet, définies aléatoirement
+	@param[in] pj - paramètres de  jeu (nb lignes/colonnes)
+*/
 void poserObjet(Grille &gr, Position &pos, const ParamsJeu &pj)
 {
     pos.x = aleatoire(0,pj.nbCol-1); // genere une position pour l'objet alÃ©atoirement
@@ -108,6 +159,10 @@ void poserObjet(Grille &gr, Position &pos, const ParamsJeu &pj)
     }
 }
 
+/**
+	Permet de poser nObjets dans la grille. Appelle nObjets fois la fonctions poserObjet
+	@param(in,out] gr - grille dans laquelle poser les objets
+*/
 void poserObjetsGrille( Grille &gr, int nObjets, const ParamsJeu &pj)
 {
     Position pos;
@@ -122,6 +177,13 @@ void poserObjetsGrille( Grille &gr, int nObjets, const ParamsJeu &pj)
 
 
 //-------------------------------------- Affichage de la grille --------------------------------
+/**
+	Indique le symbole d'une case donnée aux coordonnées x et y
+	@param[in] y - coordonnée y (ordonnée)
+	@param[in] x - coordonnée x (abscisse)
+	@param[in] gr - grille dont on vérifie le symbole d'une case aux coordonnées x et y
+	@return char - retourne le symbole de la case [ '/' , '\' , '.' ]
+*/
 char symboleCase( int y, int x,  const Grille & gr)
 {
     char c;
@@ -142,6 +204,11 @@ char symboleCase( int y, int x,  const Grille & gr)
     return c;
 }
 
+/**
+	Affiche la grille à l'écran dans la console
+	@param[in] gr - grille à afficher
+	@param[in] pj - paramètres de jeu (nb lignes/colonnes)
+*/
 void afficherGrille(const Grille &gr, const ParamsJeu &pj)
 {
     cout << ".  ";
@@ -177,13 +244,27 @@ void afficherGrille(const Grille &gr, const ParamsJeu &pj)
 
 
 
-//-------------------------------------- Placement des miroirs --------------------------------
+//-------------------------------------- Placement des miroirs ------------------------------------
+/**
+	Indique si une position donnée est jouable : c'est à dire si cette position dans la grille est libre et valide
+	@param[in] gr - grille
+	@param[in] pos - position (coordonnées)
+	@param[in] pj - paramètres de jeu (utilisé dans la fonction estValide)
+	@return boolean - renvoie vrai si la case est jouable, faux sinon
+*/
 bool estJouable(const Grille &gr, const Position &pos, const ParamsJeu &pj)
 {
 
     return (estLibre(pos,gr) && estValide(pos,gr, pj));
 }
 
+/**
+	Permet de saisir la position d'un miroir à placer dans la grille
+	@param[in] gr - grille dans laquelle on placera le miroir
+	@param[out] pos - position que saisit l'utilisateur afin de placer un miroir
+	@param[in] pj - paramètres de jeu (utilisé dans la fonction estJouable
+	@return Position - retourne la position à laquelle on peut placer le miroir, si la case est jouable
+*/
 Position saisirMiroir(const Grille &gr, Position &pos, const ParamsJeu &pj) //
 {
     cout << "Entrer x : " ;
@@ -196,13 +277,18 @@ Position saisirMiroir(const Grille &gr, Position &pos, const ParamsJeu &pj) //
     if (estJouable(gr,pos,pj)){
         return pos;
     }else{
-        cout << "Position invalide, rÃ©Ã©ssayez." << endl;
+        cout << "Position invalide, reessayez." << endl;
         saisirMiroir( gr, pos, pj);
     }
 
 
 }
 
+/**
+	Demande à l'utilisateur quel type de miroir il veut placer : '/' ou '\'
+	@param[out] miroir - miroir que l'utilisateur définit
+	@return int - retourne le miroir que l'utilisateur a saisi
+*/
 int saisieTypeMiroir(int &miroir)
 {
     cout << "Saisir un type de miroir : " << endl << "-1 = /" << endl << "-2 = \\" << endl;
@@ -215,7 +301,12 @@ int saisieTypeMiroir(int &miroir)
     return miroir;
 }
 
-
+/**
+	Permet de poser nbMiroir : paramètre que l'utilisateur renseigne avant de commencer la partie
+	@param[in] pj - paramètres de jeu (nbMiroir)
+	@param[in,out] gr - grille dans laquelle on pose un miroir
+	@param[in,out] pos - position à laquelle l'utilisateur place le miroir
+*/
 void poserMiroirs(const ParamsJeu &pj, Grille &gr, Position &pos)
 {
     int m;
@@ -231,12 +322,22 @@ void poserMiroirs(const ParamsJeu &pj, Grille &gr, Position &pos)
 
 
 //-------------------------------------- Effacement des miroirs --------------------------------
+/**
+	Indique une position donné dans la grille est un miroir
+	@param[in] pos - coordonnées
+	@param[in] gr - grille
+	@return boolean - true si la position dans la grille correspond à un miroir, false sinon
+*/
 bool estMiroir(const Position &pos,const Grille &gr)
 {
     return ((gr.tab[pos.y][pos.x] == VMIROIR1) || (gr.tab[pos.y][pos.x] == VMIROIR2));
 }
 
-
+/**
+	Permet d'effacer tous les miroirs de la grille passée en paramètres
+	@param[in,out] gr - grille
+	@param[in] pj - paramètres de jeu (nb lignes/colonnes)
+*/
 void effacerMiroirs(Grille &gr, const ParamsJeu &pj)
 {
     Position pos;
@@ -259,6 +360,11 @@ void effacerMiroirs(Grille &gr, const ParamsJeu &pj)
 
 
 //-------------------------------------- Modelisation du parcours --------------------------------
+/**
+	Affiche la grille de parcours du robot
+	@param[in,out] grv - grille visite : grille de parcours du robot
+	@param[in] pj - paramètres de jeu (nb lignes/colonnes)
+*/
 void afficherGrilleParcours(const GrilleVisite &grv, const ParamsJeu &pj)
 {
     cout << ".  ";
@@ -293,8 +399,14 @@ void afficherGrilleParcours(const GrilleVisite &grv, const ParamsJeu &pj)
     cout << endl;
 }
 
-
-void majGrilleVisite(const Position &pos, GrilleVisite &grv, int dirx, int diry)  //
+/**
+	Permet de mettre à jour la grille de parcours du robot par les valeurs 1 ou 2 selon dirx et diry donnés en paramètres
+	@param[in] pos - position dans la grille de parcours
+	@param[in,out] grv - grille de parcours du robot
+	@param[in] dirx - direction x (abscisse) du robot. 1 si le robot se déplace vers la droite, 0 s'il ne se déplace pas selon l'axe des abscisses, -1 s'il se déplace vers la gauche
+	@param[in] diry - direction y (ordonnée) du robot. 1 si le robot se déplace vers le bas, 0 s'il ne se déplace pas selon l'axe des ordonnées, -1 s'il se déplace vers le haut
+*/
+void majGrilleVisite(const Position &pos, GrilleVisite &grv, int dirx, int diry)
 {
     if ( dirx == 0) {
         grv.tab[pos.y][pos.x] = EVERTICAL;
@@ -305,14 +417,22 @@ void majGrilleVisite(const Position &pos, GrilleVisite &grv, int dirx, int diry)
     }
 }
 
-
-//fait a la maison
+/**
+	Indique si une position dans la grille visite a déjà été visité
+	@param[in] grv - grille de parcours du robot
+	@param[in] pos - position
+	@return boolean - true si la case est déjà visité (!=0), false sinon
+*/
 bool estDejaVisite(const GrilleVisite &grv, const Position &pos)
 {
     return (grv.tab[pos.y][pos.x] != 0 ); // verifie si la case est diffÃ©rente de 0
 }
 
-
+/**
+	Permet de permuter deux variables d'entier passées en paramètre
+	@param[in,out] entier1 - 1er entier
+	@param[in,out] entier2 - 2e entier
+*/
 void permuter2i(int &entier1, int &entier2)
 {
     int inter = entier1;
@@ -320,7 +440,13 @@ void permuter2i(int &entier1, int &entier2)
     entier2= inter;
 }
 
-
+/**
+	Permet de changer la direction du parcours du robot dans la grille visite selon un miroir
+	@param[in] grv - grille de parcours du robot
+	@param[in] miroir - miroir selon lequel la direction changera
+	@param[in] diry - direction y du robot
+	@param[in] dirx - direction x du robot
+*/
 void changerDir(GrilleVisite &grv, int miroir, int &diry,  int &dirx)
 {
     if (miroir == VMIROIR1)
@@ -341,13 +467,24 @@ void changerDir(GrilleVisite &grv, int miroir, int &diry,  int &dirx)
 
 
 //-------------------------------------- Parcours du robot --------------------------------
+/**
+	Indique si une position dans la grille est un objet ou non
+	@param[in] gr - grille dans laquelle on vérifie les coordonnées
+	@param[in] pos - position dans la grille
+	@return boolean - true si la position dans la grille est un objet, false sinon
+*/
 bool estObjet(const Grille &gr, const Position&pos )
 {
 
     return (gr.tab[pos.y][pos.x] != 0 && gr.tab[pos.y][pos.x] != VMIROIR1 && gr.tab[pos.y][pos.x] != VMIROIR2) ;
 }
 
-
+/**
+	Indique si le robot termine son parcours par la dernière case de la grille (en bas à droite)
+	@param[in] gr - grille
+	@param[in] pos - position
+	@param[in] pj - paramètres de jeu (nb lignes/colonnes)
+*/
 bool estPosFinValide(const Grille &gr, const Position &pos, const ParamsJeu &pj)
 {
     if ( gr.tab[pj.nbLignes][pj.nbCol] == VMIROIR2 ||  gr.tab[pj.nbLignes][pj.nbCol] == VMIROIR1)
@@ -358,7 +495,12 @@ bool estPosFinValide(const Grille &gr, const Position &pos, const ParamsJeu &pj)
 
 }
 
-
+/**
+	Permet d'avancer la position du robot dans la grille visite
+	@param[in,out] pos - position du robot, qui s'incrémente ou se décrémente
+	@param[in] diry - direction y du robot
+	@param[in] dirx - direction x du robot
+*/
 void avancerPos(Position &pos, int diry, int dirx)
 {
     switch (dirx)
@@ -385,6 +527,14 @@ void avancerPos(Position &pos, int diry, int dirx)
     }
 }
 
+/**
+	Permet de réaliser le parcours du robot
+	@param[in] gr - grille
+	@param[in,out] grv - grille visite (parcours du robot)
+	@param[in] rp - RParcours : renseigne les informations relative au joueur (nb objets, pts totaux, bonus)
+	@param[in] nmiroirs - nombre de miroirs sur lesquels le robot est passé
+	@param[in] pj - paramètres de jeu (nb lignes/colonnes)
+*/
 void parcoursRobot(Grille &gr, GrilleVisite &grv,RParcours &rp, int nmiroirs, const ParamsJeu &pj)
 {
     Position pos;
@@ -442,19 +592,35 @@ void parcoursRobot(Grille &gr, GrilleVisite &grv,RParcours &rp, int nmiroirs, co
 
 
 //------------------------------------ JOUEURS -----------------------------------------------------------
+/**
+	Initialise le score et le nombre de joueurs
+	@param[in] pj - paramètres de jeu (nbJoueur)
+	@param[in,out] score - score à initialiser
+*/
 void initialiserScores(const ParamsJeu &pj,Scores &score)
 {
     score.score = 0;
     score.nbJoueur = pj.nbJoueurs;
 }
 
-void actualiserScore(int joueurs[], const ParamsJeu &pj,Scores &score, int joueurCourant)
+/**
+	Permet d'actualiser le score d'un joueur donné
+	@param[in,out] joueurs - tableau dans lequel sont renseignés tous les joueurs de la partie
+	@param[in] pj - paramètres de jeu
+	@param[int,out] score - score à incrémenter
+	@param[in] joueurCourant - numéro du joueur dont il faut incrémenter le score
+*/
+void actualiserScore(int joueurs[], const ParamsJeu &pj, Scores &score, int joueurCourant)
 {
     joueurs[joueurCourant] += score.score;
     score.score = 0;
 }
 
-
+/**
+	Permet d'afficher le gagnant de la partie
+	@param[in] joueurs - tableau des joueurs de la partie contenant les scores de chaque joueur
+	@param[in] score - renseigne le nombre de joueurs
+*/
 void afficherGagnant(int joueurs[], Scores &score )
 {
     int max = joueurs[0];
@@ -474,6 +640,10 @@ void afficherGagnant(int joueurs[], Scores &score )
 
 
 //------------------------------------------Parametres du jeu ----------------------------------------------------
+/**
+	Permet à l'utilisateur de saisir les paramètres du jeu avant de débuter la partie
+	@param[in,out] pj - paramètres de jeu (nb lignes/colonnes, nb joueurs, nb objets, nb miroirs, valeur de bonus
+*/
 void saisirParams(ParamsJeu &pj)
 {
     nbligne:
@@ -633,7 +803,11 @@ void saisirParams(ParamsJeu &pj)
 //}
 
 
-
+/**
+	Permet d'initialiser un tableau de joueurs, avec un score égale à 0
+	@param[in,out] joueur - tableau de joueurs
+	@param[in] pj - paramètres de jeu (nb lignes/colonnes)
+*/
 void initialiserJoueurs(int joueur[], ParamsJeu &pj)
 {
     for (int i = 0; i < pj.nbJoueurs; i++)
@@ -643,6 +817,9 @@ void initialiserJoueurs(int joueur[], ParamsJeu &pj)
 
 }
 
+/**
+	Permet de jouer : appel toutes les fonctions nécessaire au bon déroulement de la partie
+*/
 void jeu()
 {
     ParamsJeu pj;
